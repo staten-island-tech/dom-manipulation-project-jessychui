2d2a17410909360487341c0cf538d24fa30ec8cd
jessychui <jessy.chui@gmail.com>
2022-11-02 13:59:32 -0400

    mirnugbjhsd

---
+++ index.js
@@ -0,0 +1,7 @@
+const DOMSelectors = {
+  button: document.getElementById("butt"),
+  text: document.querySelector("#text"),
+  empty: document.querySelector(".nothing"),
+};
+
+console.log(DOMSelectors);
