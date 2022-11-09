cd3dd72c7a368a9ec01729bd9f47e16e09ced326
jessychui <jessy.chui@gmail.com>
2022-11-09 14:00:26 -0500

    im so confused but okay :-(

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const DOMSelectors = {
   button: document.getElementById("btn"),
   box: document.getElementById("container"),
   input: document.querySelector("#input"),
+  cat: document.querySelector("cat-box"),
 };
 
 DOMSelectors.button.addEventListener("click", function () {
   let input = DOMSelectors.input.value;
-  DOMSelectors.box.insertAdjacentHTML("beforeend", `<p>${input}</p>`);
+  DOMSelectors.box.insertAdjacentHTML("afterend", DOMSelectors.cat);
   DOMSelectors.input.value = "";
 });
+
+function catinsert(catholder, deletebutton) {}
