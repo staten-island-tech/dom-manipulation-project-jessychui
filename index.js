d3b25bff18aa99bc014618b8045197f230aebb59
jessychui <jessy.chui@gmail.com>
2022-11-16 14:00:38 -0500

    plea for help

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const DOMSelectors = {
-  button: document.getElementById("enter-button"),
+  enter: document.getElementById("enter-button"),
   box: document.getElementById("container"),
-  input: document.querySelector("#input"),
-  cat: document.querySelector("cat-box"),
+  nameinput: document.querySelector("#cat-name"),
+  personalityinput: document.querySelector("#cat-personality"),
+  urlinput: document.querySelector("#url"),
+  delete: document.getElementById("#remove-btn"),
 };
 
 function makeCard() {}
+
+DOMSelectors.enter.addEventListener("click", function makeCard() {});
 
 Document.getElementById("form").addEventListener("submit", function (e) {
   e.preventDefault();
