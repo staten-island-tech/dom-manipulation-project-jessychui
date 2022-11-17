210f53d6e89cf1371a4bccaa3aff7a5b1c65bf4f
jessychui <jessy.chui@gmail.com>
2022-11-17 14:01:06 -0500

    HELP

--- index.js
+++ index.js
@@ -2,15 +2,27 @@ const DOMSelectors = {
   enter: document.getElementById("enter-button"),
   box: document.getElementById("container"),
   nameinput: document.querySelector("#cat-name"),
-  personalityinput: document.querySelector("#cat-personality"),
   urlinput: document.querySelector("#url"),
+  personalityinput: document.querySelector("#cat-personality"),
   delete: document.getElementById("#remove-btn"),
+  form: document.getElementById("form"),
 };
 
-function makeCard() {}
+let nameinput = DOMSelectors.nameinput.value;
+let urlinput = DOMSelectors.urlinput.value;
+let personalityinput = DOMSelectors.personalityinput.value;
 
-DOMSelectors.enter.addEventListener("click", function makeCard() {});
-
-Document.getElementById("form").addEventListener("submit", function (e) {
+DOMSelectors.form.addEventListener("submit", function (e) {
   e.preventDefault();
+  DOMSelectors.box.insertAdjacentHTML(
+    "afterend",
+    `<section class="cat-box" id="cat-box">
+  <div class="display-card">
+  <h2 class="cat-box-name">${nameinput}</h2>
+  <img class="display-cat" src="${urlinput}"/>
+  <p class="cat-box-personality">${personalityinput}</p>
+  <button id="remove-btn">Remove Cat :-(</button>
+  </div> 
+</section>`
+  );
 });
